9baf62c6ef94e6f374c870681bd059bc884ec6ef
Ryan Lee <gxlhybh@gmail.com>
2018-01-15 00:30:22 +0800

    Migrate from Jade to Pug fix some bugs. Add experimental homepage

---
+++ routes/homepage.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router=express.Router();
+router.get("/", function (req, res) {
+	res.render("homepage", {title: "CUP Online Judge", OJ_NAME: "CUP Online Judge"});
+});
+
+module.exports=router;
