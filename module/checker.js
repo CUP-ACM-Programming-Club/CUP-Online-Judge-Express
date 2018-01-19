1ca80db7396446ab4a1f5d200a40f32027dc6fc5
Ryan Lee <gxlhybh@gmail.com>
2018-01-19 11:27:54 +0800

    complete checker

--- module/checker.js
+++ module/checker.js
@@ -2,17 +2,40 @@ const Promise = require("bluebird");
 const fs = Promise.promisifyAll(require("fs"));
 const path = require("path");
 
-async function fileToRawText(...file){
-	let result = [];
-	for(let i in file){
-		result.push({
-			name:path.basename(file),
-			data:await fs.readFileAsync(file[i]).toString()
-		});
-	}
-	return result;
+function flipSuffix(file_name) {
+    if (file_name && file_name.length) {
+        let pos;
+        if ((pos = file_name.indexOf(".")) > 0) {
+            return file_name.substring(0, pos);
+        }
+        else {
+            return file_name;
+        }
+    }
 }
 
-function compareDiff(result,...file_name){
-
+async function fileToRawText(...file) {
+    let result = [];
+    for (let i in file) {
+        result[path.basename(file)] = await fs.readFileAsync(file[i]).toString();
+    }
+    return result;
 }
+
+async function compareDiff(result, ...file_name) {
+    const _result = JSON.parse(JSON.stringify(result));
+    let userOutputText = [];
+    _result.map((file, index) => {
+        userOutputText[index] = file;
+    });
+    const rawText = await fileToRawText(...file_name);
+    let outputText = [];
+    rawText.map((file,index) => {
+        outputText[index] = file;
+    });
+}
+
+module.exports = {
+    compareDiff: compareDiff,
+    fileToRawText: fileToRawText
+};
