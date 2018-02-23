827592c0ffbfc917105f10af95ca2d138535e6fb
Ryan Lee <gxlhybh@gmail.com>
2018-02-24 02:27:19 +0800

    update status API

--- routes/status.js
+++ routes/status.js
@@ -2,18 +2,29 @@ const express = require("express");
 const router = express.Router();
 const query = require("../module/mysql_cache");
 const escape = require("escape-html");
-
-router.get("/", async function (req, res, next) {
+const const_name = require("../module/const_name");
+async function get_status(req,res,next,request_query = {},limit = 0){
 	let _res;
+	let where_sql = "";
+	let sql_arr = [];
+	for(let i in request_query){
+		if(request_query[i]) {
+			where_sql += ` and ${i} = ?`;
+			sql_arr.push(request_query[i]);
+		}
+	}
+	sql_arr.push(limit);
 	if (req.session.isadmin) {
-		console.log('isadmin');
-		_res = await query(`SELECT * FROM solution LEFT JOIN sim ON sim.s_id = solution.solution_id 
-		 ORDER BY solution.solution_id DESC LIMIT 20`);
+		_res = await query(`select * from (SELECT * FROM solution where 1=1 
+		 ${where_sql} ) sol
+		 LEFT JOIN sim ON sim.s_id = sol.solution_id 
+		 ORDER BY sol.solution_id DESC LIMIT ? , 20`,sql_arr);
 	}
 	else {
-		_res = await query(`select * from (SELECT * FROM solution where problem_id>0 and contest_id is null) sol 
+		_res = await query(`select * from (SELECT * FROM solution where problem_id>0 and contest_id is null 
+		${where_sql} ) sol 
 		LEFT JOIN sim ON sim.s_id = sol.solution_id 
-		ORDER BY sol.solution_id DESC LIMIT 20`);
+		ORDER BY sol.solution_id DESC LIMIT ?, 20`,sql_arr);
 	}
 	let result = [];
 	for (const val of _res) {
@@ -32,7 +43,46 @@ router.get("/", async function (req, res, next) {
 			judger: val.judger
 		});
 	}
-	res.json(result);
+	res.json({
+		result:result,
+		const_list:const_name,
+		self:req.session.user_id,
+		isadmin:req.session.isadmin
+	});
+}
+
+router.get("/", async function (req, res, next) {
+	await get_status(req,res,next);
+});
+
+router.get("/:problem_id/:user_id/:language/:result/:limit",async function(req,res,next){
+	const problem_id = req.params.problem_id === "null"?"":parseInt(req.params.problem_id);
+	const user_id = req.params.user_id === "null"?"":req.params.user_id;
+	const language = req.params.language === "null"?"":parseInt(req.params.language);
+	const result = req.params.result === "null"?"":parseInt(req.params.result);
+	const limit = req.params.limit === "null"? 0:parseInt(req.params.limit);
+	await get_status(req,res,next,{
+		problem_id:problem_id,
+		user_id:user_id,
+		language:language,
+		result:result
+	},limit);
+});
+
+router.get("/:problen_id/:user_id/:language/:result/:limit/:contest_id",async function(req,res,next){
+	const problem_id = req.params.problem_id === "null"?"":req.params.problem_id.toUpperCase().charCodeAt(0)-"A".charCodeAt(0);
+	const user_id = req.params.user_id === "null"?"":req.params.user_id;
+	const language = req.params.language === "null"?"":parseInt(req.params.language);
+	const result = req.params.result === "null"?"":parseInt(req.params.result);
+	const contest_id = req.params.result === "null"?"":parseInt(req.params.contest_id);
+	const limit = req.params.limit === "null"? 0:parseInt(req.params.limit);
+	await get_status(req,res,next,{
+		num:problem_id,
+		user_id:user_id,
+		language:language,
+		result:result,
+		contest_id:contest_id
+	},limit)
 });
 
 router.get("/:sid/:tr", function (req, res, next) {
