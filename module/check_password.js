2d23b2f7f9ded590af213dcd0215736d8aeedee2
Ryan Lee <gxlhybh@gmail.com>
2018-03-16 00:32:28 +0800

    Use localJudge for frontend Fix Bugs: check_password module doesn't work because password encrypted in php doesn't match the password which encrypted in Node.js environment.

--- module/check_password.js
+++ module/check_password.js
@@ -1,9 +1,14 @@
 const crypto = require("crypto");
 const checkPassword = (originalPassword, inputPassword, newpassword) => {
-	const convertedOriginalPassword = new Buffer(originalPassword, "base64").toString("utf-8");
-	const salt = convertedOriginalPassword.substring(20);
-	const SHA1Password = crypto.createHash("sha1").update(crypto.createHash("md5").update(inputPassword).digest("hex") + salt).digest() + salt;
-	return SHA1Password.substring(0, 30) === convertedOriginalPassword.substring(0, 30) || newpassword === inputPassword;
+	const convertedOriginalPasswordAscii = new Buffer(originalPassword, "base64").toString("ascii");
+	const convertedOriginalPassword = new Buffer(originalPassword, "base64").toString();
+	const salt = convertedOriginalPasswordAscii.substring(20);
+	const SHA1Password = crypto.createHash("sha1")
+		.update(crypto.createHash("md5")
+			.update(inputPassword)
+			.digest("hex") + salt)
+		.digest() + salt;
+	return SHA1Password === convertedOriginalPassword || newpassword === inputPassword;
 };
 
 module.exports = checkPassword;
