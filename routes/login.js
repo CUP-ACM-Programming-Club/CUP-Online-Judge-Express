75f8313a588536ee1a493f7b68fdf6aed2c4a97a
Ryan Lee <gxlhybh@gmail.com>
2018-03-21 22:47:08 +0800

    Fix bugs in login.js because of privilege

--- routes/login.js
+++ routes/login.js
@@ -109,11 +109,26 @@ router.post("/", async function (req, res) {
 					}
 					req.session.user_id = user_id;
 					req.session.auth = true;
-					res.json(ok.ok);
-					await query("select count(1) as count from privilege where user_id=? and rightstr='administrator'", [user_id])
-						.then((val) => {
-							req.session.isadmin = parseInt(val[0].count) > 0;
-						});
+					req.session.contest = {};
+					req.session.contest_maker = {};
+					req.session.problem_maker = {};
+					let val;
+					//for session admin privilege
+					val = await query("select rightstr from privilege where user_id = ?", [user_id]);
+					for (let i of val) {
+						if (i.rightstr === "administrator") {
+							req.session.isadmin = true;
+						}
+						else if (i.rightstr.indexOf("c") !== -1) {
+							req.session.contest[i.rightstr] = true;
+						}
+						else if (i.rightstr.indexOf("m") === 0) {
+							req.session.contest_maker[i.rightstr] = true;
+						}
+						else if (i.rightstr.indexOf("p") === 0) {
+							req.session.problem_maker[i.rightstr] = true;
+						}
+					}
 				}
 				else {
 					res.json(error.invalidUser);
