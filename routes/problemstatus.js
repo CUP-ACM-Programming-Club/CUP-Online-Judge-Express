71abbc8615cfe45913fe0ce30bc2f7fe8b314940
Ryan Lee <gxlhybh@gmail.com>
2018-06-19 01:40:04 +0800

    Update problem staus API

--- routes/problemstatus.js
+++ routes/problemstatus.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const [error] = require("../module/const_var");
 const auth = require("../middleware/auth");
 const cache_query = require("../module/mysql_cache");
+const const_variable = require("../module/const_name");
 const getProblemStatus = async (req, res, opt = {source: true, id: 0, page: 0, from: "", page_cnt: 20}) => {
 	if (opt.id === 0) {
 		res.json(error.invalidParams);
@@ -23,7 +24,7 @@ const getProblemStatus = async (req, res, opt = {source: true, id: 0, page: 0, f
 		if (opt.source) {
 			from = "solution";
 		}
-		const [_result, solution] = await Promise.all([cache_query(`select count(1) total,result from ${from}
+		const [_result, solution, _total, _solved] = await Promise.all([cache_query(`select count(1) total,result from ${from}
 		 where problem_id = ? ${hasOJName(opt.source)}
         group by result`, [opt.id, opt.from]),
 		cache_query(`select user_id,solution_id,language,code_length,in_date,time,memory from ${from}
@@ -35,12 +36,22 @@ order by time,memory,code_length,in_date,solution_id limit ?,?`, (() => {
 			else {
 				return [opt.id, opt.page * opt.page_cnt, opt.page_cnt];
 			}
-		})())]);
+		})()),
+		cache_query("select count(1) total,count(distinct user_id)solvesubmit from solution where problem_id = ?", [opt.id]),
+		cache_query(`select count(distinct user_id)solveuser from solution where problem_id = ?
+and result = 4`, [opt.id])]);
+		const submit_status = {
+			total_submit: _total[0].total,
+			total_solved_submit: _total[0].solvesubmit,
+			total_solved_user: _solved[0].solveuser
+		};
 		res.json({
 			status: "OK",
 			data: {
 				problem_status: _result,
-				solution_status: solution
+				solution_status: solution,
+				submit_status: submit_status,
+				statistic_name: const_variable.result.cn
 			}
 		});
 	}
