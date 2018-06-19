d487dda0d4232a72a6f7cd7345e20cd57ab85d60
Ryan Lee <gxlhybh@gmail.com>
2018-06-19 23:47:13 +0800

    Add cache timestamp Update problemstatus to create Chart

--- routes/problemstatus.js
+++ routes/problemstatus.js
@@ -25,7 +25,30 @@ const getProblemStatus = async (req, res, opt = {source: true, id: 0, page: 0, f
 		if (opt.source) {
 			from = "solution";
 		}
-		const [_result, solution, _total, _solved, _passed, _end_contest] = await Promise.all([cache_query(`select count(1) total,result from ${from}
+		const problem_limit = await cache_query("select time_limit,memory_limit from problem where problem_id = ?", [opt.id]);
+		const time_limit = problem_limit[0].time_limit * 1000;
+		const memory_limit = problem_limit[0].memory_limit * 1024;
+		const time_step = parseFloat(time_limit) * 2 / 25;
+		const memory_step = parseFloat(memory_limit) * 2 / 25;
+		let sql1 = `select count(1)total,diff from
+  (select case`;
+		for (let i = 0; i <= time_limit; i += time_step) {
+			sql1 += ` when time between ${parseInt(i)} and ${parseInt(i + time_step)} then '${parseInt(i)}-${parseInt(i + time_step)}' `;
+		}
+
+		sql1 += `else '>${time_limit}' end as diff `;
+		sql1 += ` from solution where problem_id = ? and result = 4)t
+group by diff`;
+		let sql2 = `select count(1)total,diff from
+  (select case`;
+		for (let i = 0; i <= memory_limit; i += memory_step) {
+			sql2 += ` when memory between ${parseInt(i)} and ${parseInt(i + memory_step)} then '${parseInt(i)}-${parseInt(i + memory_step)}' `;
+		}
+
+		sql2 += `else '>${time_limit}' end as diff `;
+		sql2 += ` from solution where problem_id = ? and result = 4)t
+group by diff`;
+		const [_result, solution, _total, _solved, _passed, _end_contest, _time_range, _memory_range] = await Promise.all([cache_query(`select count(1) total,result from ${from}
 		 where problem_id = ? ${hasOJName(opt.source)}
         group by result`, [opt.id, opt.from]),
 		cache_query(`select user_id,solution_id,language,code_length,in_date,time,memory from ${from}
@@ -45,7 +68,9 @@ and result = 4`, [opt.id]),
 			req.session.user_id]),
 		cache_query(`select max(end_time) as end_time from contest where contest_id in
                             (select contest_problem.contest_id from contest_problem
-                            where problem_id = ?)`, [opt.id])]);
+                            where problem_id = ?)`, [opt.id]),
+		cache_query(sql1, [opt.id]),
+		cache_query(sql2, [opt.id])]);
 		let browse_privilege = req.session.isadmin || req.session.source_browser;
 		let after_contest = true;
 		if (_end_contest.length > 0) {
@@ -69,7 +94,7 @@ and result = 4`, [opt.id]),
 				i.memory = "----";
 			}
 		}
-		res.json({
+		let sendJSON = {
 			status: "OK",
 			data: {
 				problem_status: _result,
@@ -80,7 +105,12 @@ and result = 4`, [opt.id]),
 				isadmin: req.session.isadmin || req.session.source_browser,
 				self: req.session.user_id
 			}
-		});
+		};
+		if (browse_privilege) {
+			sendJSON.data.time_range = _time_range;
+			sendJSON.data.memory_range = _memory_range;
+		}
+		res.json(sendJSON);
 	}
 };
 
