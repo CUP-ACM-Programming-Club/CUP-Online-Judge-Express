068fe934680e934ba3db710e31333d153eac6c9e
Ryan Lee <gxlhybh@gmail.com>
2018-06-28 03:10:44 +0800

    Update acmmanager API Update status API Modify Judger database picker

--- routes/acmsubmit.js
+++ routes/acmsubmit.js
@@ -1,26 +1,44 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
-const dayjs = require("dayjs");
+const const_variable = require("../module/const_name");
 const cache_query = require("../module/mysql_cache");
-const query = require("../module/mysql_query");
 router.get("/", async (req, res) => {
-	let sql = `select t.*,users.nick from(select * from vjudge_record
+	let wheresql = "";
+	let andsql = "";
+	let sqlArr = [];
+	if (req.query.name) {
+		wheresql = " where user_id = ? ";
+		andsql = " and user_id = ? ";
+		for (let i = 0; i < 3; ++i) {
+			sqlArr.push(req.query.name);
+		}
+	}
+
+	let sql = `select t.*,users.nick from(select user_id,oj_name,problem_id,time,result,time_running,
+	memory,code_length,language
+	 from vjudge_record 
+	 ${wheresql}
 union all
-    select user_id,oj_name,problem_id,in_date as time from vjudge_solution
-      where user_id in (select user_id from users_account)
-      group by CONCAT(oj_name,problem_id)
+    select user_id,oj_name,problem_id,in_date as time,result,time as time_running
+    ,memory,code_length,language
+    from vjudge_solution 
+      where user_id in (select user_id from users_account) ${andsql}
   union all
-  select user_id,"LOCAL" as oj_name,problem_id,in_date as time from solution
-  where user_id in (select user_id from users_account) and problem_id > 1909
-  group by problem_id
+  select user_id,"LOCAL" as oj_name,problem_id,in_date as time,result,time as time_running
+    ,memory,code_length,language from solution
+  where user_id in (select user_id from users_account) and problem_id > 1909 ${andsql}
 order by time desc limit 500)t
 left join users
   on users.user_id = t.user_id`;
-	const data = await query(sql);
+	const data = await cache_query(sql,sqlArr);
 	res.json({
 		status: "OK",
-		data: data
+		data: data,
+		const_data: const_variable.result.cn,
+		language_name: const_variable.language_name,
+		judge_color:const_variable.judge_color,
+		icon_list:const_variable.icon_list
 	});
 });
 
