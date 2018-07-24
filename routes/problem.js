d4669cd83b98343f6b51ef523d807955e2cd6688
Ryan Lee <gxlhybh@gmail.com>
2018-07-24 14:42:08 +0800

    Add PyPy and PyPy3 support

--- routes/problem.js
+++ routes/problem.js
@@ -287,9 +287,15 @@ router.get("/:source/:id", function (req, res) {
 	const id = parseInt(req.params.id);
 	const _res = cache.get("source/id/" + source + id);
 	if (_res === undefined) {
+		if(process.env.NODE_ENV === "test") {
+			console.log("match cache");
+		}
 		make_cache(res, req, {problem_id: id, source: source});
 	}
 	else {
+		if(process.env.NODE_ENV === "test") {
+			console.log("not match");
+		}
 		send_json(res, _res);
 		make_cache(null, req, {problem_id: id, source: source});
 	}
