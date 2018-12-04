a486b06b218769f30837a5f012d51a1e379f2dbe
Ryan Lee <gxlhybh@gmail.com>
2018-12-04 21:44:13 +0800

    Fix session data lost after restart program

--- module/session.js
+++ module/session.js
@@ -1,13 +1,14 @@
 const session = require("express-session");
 const RedisStore = require("connect-redis")(session);
 const Random = require("meteor-random");
+const secretKey = require("../config.json").session_secret || Random.secret(128);
 const oneDay = 86400000;
 const sessionMiddleware = session({
 	store: new RedisStore,
 	saveUninitialized: false,
 	ttl: oneDay * 31 * 12 * 100,
 	resave: false,
-	secret: Random.secret(128),
+	secret: secretKey,
 	cookie: {
 		maxAge: 60 * 60 * 1000
 	}
