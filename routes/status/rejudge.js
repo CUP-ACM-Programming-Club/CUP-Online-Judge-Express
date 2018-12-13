b9c64eed8ce2ac2b8c21ef980008b654114a3912
Ryan Lee <gxlhybh@gmail.com>
2018-12-13 20:05:14 +0800

    Fix format Remove dumplicate function Reformat code style

--- routes/status/rejudge.js
+++ routes/status/rejudge.js
@@ -4,18 +4,18 @@ const [error, ok] = require("../../module/const_var");
 const query = require("../../module/mysql_query");
 
 router.post("/", async (req, res) => {
-    try {
-        const solution_id = req.body.solution_id;
-        if (typeof solution_id === "undefined" || isNaN(solution_id)) {
-            res.json(error.solutionIdNotValid);
-        } else {
-            await query("update solution set result = 1 where solution_id = ?", [solution_id]);
-            res.json(ok.ok);
-        }
-    } catch (e) {
-        console.log(e);
-        res.json(error.database);
-    }
+	try {
+		const solution_id = req.body.solution_id;
+		if (typeof solution_id === "undefined" || isNaN(solution_id)) {
+			res.json(error.solutionIdNotValid);
+		} else {
+			await query("update solution set result = 1 where solution_id = ?", [solution_id]);
+			res.json(ok.ok);
+		}
+	} catch (e) {
+		console.log(e);
+		res.json(error.database);
+	}
 });
 
 module.exports = router;
