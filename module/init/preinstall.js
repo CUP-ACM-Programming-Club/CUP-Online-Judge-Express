1bc5b4aeb0cb97ff4760220ef6821f2dcab62d95
Ryan Lee <gxlhybh@gmail.com>
2018-12-30 16:37:21 +0800

    Wrong API called

--- module/init/preinstall.js
+++ module/init/preinstall.js
@@ -2,7 +2,7 @@ const ENVIRONMENT = process.env.NODE_ENV;
 const fs = require("fs");
 module.exports = function(){
 	if(ENVIRONMENT === "test") {
-		if(!fs.accessSync("../../config.json")) {
+		if(!fs.existsSync("../../config.json")) {
 			fs.copyFileSync("../../config-sample.json", "../../config.json");
 		}
 	}
