67c417923c463549b29c1308369361a6a09447bc
Ryan Lee <gxlhybh@gmail.com>
2018-12-30 16:45:39 +0800

    modified

--- module/init/preinstall.js
+++ module/init/preinstall.js
@@ -1,9 +1,12 @@
 const ENVIRONMENT = process.env.NODE_ENV;
 const fs = require("fs");
+const path = require("path");
+const config_path = path.join(process.cwd(),"config.json");
+const config_sample_path = path.join(process.cwd(),"config-sample.json");
 module.exports = function(){
 	if(ENVIRONMENT === "test") {
-		if(!fs.existsSync("../../config.json")) {
-			fs.copyFileSync("../../config-sample.json", "../../config.json");
+		if(!fs.existsSync(config_path)) {
+			fs.copyFileSync(config_sample_path,config_path);
 		}
 	}
 };
