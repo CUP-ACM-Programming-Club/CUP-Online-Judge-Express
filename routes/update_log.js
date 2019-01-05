63bc0c88c6d944924369f27b880f14fdb219c24e
Ryan Lee <gxlhybh@gmail.com>
2019-01-06 00:41:54 +0800

    Fix wrong router

--- routes/update_log.js
+++ routes/update_log.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
 const cache_query = require("../module/mysql_cache");
 
 function getMaintainInfo(limit = undefined) {
-	return cache_query(`select * from maintain_info ${limit ? "limit 1" : ""}`);
+	return cache_query(`select * from maintain_info order by mtime desc ${limit ? "limit 1" : ""}`);
 }
 
 router.get("/", async (req, res) => {
@@ -13,11 +14,12 @@ router.get("/", async (req, res) => {
 	});
 });
 
-router.get("/", async (req, res) => {
+
+router.get("/latest", async (req, res) => {
 	res.json({
 		status: "OK",
 		data: await getMaintainInfo(true)
 	});
 });
 
-module.exports = ["/update_log", router];
+module.exports = ["/update_log", auth, router];
