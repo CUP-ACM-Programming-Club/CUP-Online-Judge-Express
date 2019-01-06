368583a54faf813c07b57be3b33feeb42b935ed2
Ryan Lee <gxlhybh@gmail.com>
2019-01-06 16:35:46 +0800

    Complete special subject get "/" router Prepare for "/:cid" router

--- routes/specialsubject.js
+++ routes/specialsubject.js
@@ -1,13 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const query = require("../module/mysql_query");
 const cache_query = require("../module/mysql_cache");
 
 const [error] = require("../module/const_var");
 const auth = require("../middleware/auth");
 
-router.get("/", async (req, res, next) => {
+router.get("/", async (req, res) => {
+	try {
+		const sql = "SELECT * FROM `special_subject` WHERE defunct='N' ORDER BY `topic_id` ASC limit 1000";
+		const data = await cache_query(sql);
+		res.json({
+			status: "OK",
+			data
+		});
+	}
+	catch (e) {
+		console.log(e);
+		res.json(error.database);
+	}
+});
 
+router.get("/:cid", async (req, res) => {
+	try {
+		// TODO:complete cid API
+	}
+	catch(e) {
+		console.log(e);
+		res.json(error.database);
+	}
 });
 
 module.exports = ["/specialsubject", auth, router];
