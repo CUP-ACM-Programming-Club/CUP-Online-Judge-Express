7818c85ad22b9beaf0e193b75777521a9ecfa328
Ryan Lee <gxlhybh@gmail.com>
2019-01-22 00:53:24 +0800

    Cover all code in captcha checker and const_var

--- test/module/test.captcha_checker.js
+++ test/module/test.captcha_checker.js
@@ -5,4 +5,57 @@ describe("check captcha", function(){
 	it("should require a object", function () {
 		expect(checkCaptcha).to.have.property("checkCaptcha").that.is.a("function");
 	});
+
+	it("should check captcha with same case", function () {
+		const from = "test";
+		const req = {
+			session:{
+				captcha:{
+					from:"test",
+					captcha: "captcha"
+				}
+			},
+			body: {
+				captcha: "captcha"
+			}
+		};
+		expect(checkCaptcha.checkCaptcha(req, from))
+			.to.be.a("boolean")
+			.that.is.equal(true);
+	});
+
+	it("should return false if captcha from is different", function () {
+		const from = "prod";
+		const req = {
+			session:{
+				captcha:{
+					from:"test",
+					captcha: "captcha"
+				}
+			},
+			body: {
+				captcha: "captcha"
+			}
+		};
+		expect(checkCaptcha.checkCaptcha(req, from))
+			.to.be.a("boolean")
+			.that.is.equal(false);
+	});
+	it("should return true with different case but same captcha", function () {
+		const from = "test";
+		const req = {
+			session:{
+				captcha:{
+					from:"test",
+					captcha: "CapTcHa"
+				}
+			},
+			body: {
+				captcha: "captcha"
+			}
+		};
+		expect(checkCaptcha.checkCaptcha(req, from))
+			.to.be.a("boolean")
+			.that.is.equal(true);
+	});
 });
--- test/module/test.const_var.js
+++ test/module/test.const_var.js
@@ -9,6 +9,15 @@ describe("const variable and function", function() {
 	it("should construct a error object", function () {
 		expect(error.errorMaker("test"))
 			.to.have.property("statement")
-			.that.equal("test")
+			.that.equal("test");
+		expect(error.attributeMaker({test:"statement"}))
+			.to.have.property("test")
+			.that.equal("statement");
 	});
+
+	it("should construct a ok object", function() {
+		expect(ok.okMaker("test"))
+			.to.have.property("data")
+			.that.equal("test");
+	})
 });
