c4d9f342cd4c63606d1b521f1f643421d2b39c95
Ryan Lee <gxlhybh@gmail.com>
2019-01-29 02:13:10 +0800

    Fix mysql pool

--- test/module/login_action.test.js
+++ test/module/login_action.test.js
@@ -2,6 +2,7 @@ const expect = require("chai").expect;
 
 describe("login action test", function(){
     const login_action = require("../../module/login_action");
+
     it('should contained several object after login action', async function () {
         const req = {
             session: {}
@@ -12,8 +13,6 @@ describe("login action test", function(){
         expect(req.session.contest).to.be.a("object").that.is.deep.equal({});
         expect(req.session.contest_maker).to.be.a("object").that.is.deep.equal({});
         expect(req.session.problem_maker).to.be.a("object").that.is.deep.equal({});
-    });
-    after(function(){
         require("../../module/mysql_query").pool.end();
-    })
+    });
 });
