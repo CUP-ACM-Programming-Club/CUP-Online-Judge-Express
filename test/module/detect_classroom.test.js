a14c3c527d8c1b0f38c2a92a0185a7f6ab8e2669
Ryan Lee <gxlhybh@gmail.com>
2019-01-29 01:54:49 +0800

    Update unit test: Detect_classroom login_action

--- test/module/detect_classroom.test.js
+++ test/module/detect_classroom.test.js
@@ -2,12 +2,15 @@ const expect = require("chai").expect;
 
 describe("detect classroom", function () {
 	const detectClassroom = require("../../module/detect_classroom");
+	it('should return null while string does not contain dot', function () {
+		expect(detectClassroom("1111")).to.equal(null);
+	});
 	it("should return null while ip is not in classroom", function () {
 		expect(detectClassroom("127.0.0.1")).to.equal(null);
 	});
 
 	it("should return null if it's not an ip address", function () {
-		expect(detectClassroom("a.b.c.d")).to.equal(null);
+		expect(detectClassroom("10.200.c.d")).to.equal(null);
 	});
 
 	it("should return 403", function () {
---
+++ test/module/login_action.test.js
@@ -0,0 +1,19 @@
+const expect = require("chai").expect;
+
+describe("login action test", function(){
+    const login_action = require("../../module/login_action");
+    it('should contained several object after login action', async function () {
+        const req = {
+            session: {}
+        };
+        await login_action(req, "test_name");
+        expect(req.session.auth).to.equal(true);
+        expect(req.session.user_id).to.equal("test_name");
+        expect(req.session.contest).to.be.a("object").that.is.deep.equal({});
+        expect(req.session.contest_maker).to.be.a("object").that.is.deep.equal({});
+        expect(req.session.problem_maker).to.be.a("object").that.is.deep.equal({});
+    });
+    after(function(){
+        require("../../module/mysql_query").pool.end();
+    })
+});
