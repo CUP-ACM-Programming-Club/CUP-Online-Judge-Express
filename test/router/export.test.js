efa70fe2b660b08d6a1c667a3aef9d57cdd0b9e3
Ryan Lee <gxlhybh@gmail.com>
2019-04-22 22:33:36 +0800

    Fix problem in unit test. Add new unit test for scoreboard

--- test/router/export.test.js
+++ test/router/export.test.js
@@ -4,6 +4,7 @@ const [error] = require("../../module/const_var");
 async function removeAll() {
     await query("delete from users where nick = 'test' or nick = 'test1'");
     await query("delete from source_code");
+    await query("delete from privilege where user_id = 'test'");
     await query("delete from solution where user_id = 'test'");
 }
 
