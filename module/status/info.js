0ce6fdd3b3f9fbd730ea14ef1c5562f7907588e1
Ryan Lee <gxlhybh@gmail.com>
2019-05-22 20:10:43 +0800

    Fix illegal privilege

--- module/status/info.js
+++ module/status/info.js
@@ -1,12 +1,12 @@
 const query = require("../mysql_cache");
 const [error, ok] = require("../const_var");
-module.exports = async function (req, database, solution_id) {
+module.exports = async function (req, database, solution_id, own_watch = false) {
 	const user_id = req.session.user_id;
 	const infoPromise = query(`select error from ${database} where solution_id = ?`, [solution_id]);
 	const solutionPromise = query("select user_id from solution where solution_id = ?", [solution_id]);
 	const [infoRes, solutionRes] = await Promise.all([infoPromise, solutionPromise]);
 	if (solutionRes.length > 0) {
-		if (!req.session.isadmin && user_id !== solutionRes[0].user_id) {
+		if (!req.session.isadmin && (!own_watch || user_id !== solutionRes[0].user_id)) {
 			return error.noprivilege;
 		} else {
 			return ok.okMaker({
--- routes/status/compile_info.js
+++ routes/status/compile_info.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const infoHandler = require("../../module/status/info");
 router.get("/:solution_id", async (req, res) => {
 	const solution_id = req.params.solution_id;
-	res.json(await infoHandler(req, "compileinfo", solution_id));
+	res.json(await infoHandler(req, "compileinfo", solution_id, true));
 });
 
 module.exports = ["/compile_info", router];
