a1d1a4c17ee11b573d62052e9f635e9f6c910521
Ryan Lee <gxlhybh@gmail.com>
2019-09-18 16:56:15 +0800

    sync before build mysqlFactory

--- module/config/store/switch/mysql.js
+++ module/config/store/switch/mysql.js
@@ -1,5 +1,6 @@
 const sequelize = require("../../../../orm/instance/sequelize");
 const Switch = sequelize.import("switch", require("../../../../orm/models/switch"));
+Switch.sync();
 const mysqlFactory = require("../base/mysql");
 
 module.exports = mysqlFactory(Switch);
