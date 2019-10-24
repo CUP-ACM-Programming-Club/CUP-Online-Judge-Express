6b73ef5706508884f3e09d3e3fc55a8e20ecfb29
Ryan Lee <gxlhybh@gmail.com>
2019-10-24 15:51:54 +0800

    HotFix logout

--- routes/logout.js
+++ routes/logout.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
-router.get("/", function (req, res) {
+const client = require("../module/redis");
+router.get("/", async function (req, res) {
+	const userId = req.session.user_id;
 	req.session.destroy();
+	let size = await client.llenAsync(`${userId}newToken`);
+	while(size-- > 0) {
+		await client.lpopAsync(`${userId}newToken`);
+	}
+	size = await client.llenAsync(`${userId}token`);
+	while(size-- > 0) {
+		await client.lpopAsync(`${userId}token`);
+	}
 	res.json({
 		status: "OK"
 	});
