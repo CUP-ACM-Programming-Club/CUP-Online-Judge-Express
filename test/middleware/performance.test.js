2091e1365d129c98bb317e4c74df225f1f5a58d3
Ryan Lee <gxlhybh@gmail.com>
2019-10-28 18:32:15 +0800

    Add cluster-id to header

--- test/middleware/performance.test.js
+++ test/middleware/performance.test.js
@@ -6,7 +6,7 @@ res.set = function(key, value) {
 };
 describe("performance middleware", function(){
 	it("should contain a header X-Execution-Time", function () {
-		const performance = require("../../middleware/performance");
+		const performance = require("../../middleware/http_header");
 		performance({}, res, ()=>{});
 		res.send();
 		expect(res["X-Execution-Time"]).to.be.a("string");
