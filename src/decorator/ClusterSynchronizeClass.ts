207101b8fd7417be4ae9ad98ed1442c558920a24
Ryan Lee <gxlhybh@gmail.com>
2019-12-11 21:33:09 +0800

    [Fix] ES6 not allow call constructor without new

--- src/decorator/ClusterSynchronizeClass.ts
+++ src/decorator/ClusterSynchronizeClass.ts
@@ -1,34 +1,20 @@
-export default function ClusterSynchronizeClass(target: any) {
+export default function ClusterSynchronizeClass<T extends { new(...constructorArgs: any[]): any }>(constructorFunction: T) {
     // save a reference to the original constructor
-    const original = target;
-    const className = target.name;
-    // a utility function to generate instances of a class
-    function construct(constructor: any, args: any[]) {
-        const c: any = function (this: any) {
-            return constructor.apply(this, args);
+    let newConstructorFunction: any = function (this: any, ...args: any[]) {
+        let func: any = function () {
+            return new constructorFunction(...args);
         };
-        c.prototype = constructor.prototype;
-        return new c();
-    }
-
-    // the new constructor behaviour
-    const newConstructor: any = function (this: any, ...args: any[]) {
-        const instance = construct(original, args);
+        func.prototype = constructorFunction.prototype;
+        let result: any = new func();
+        const className = constructorFunction.name;
         process.on("message", (data) => {
-            if (data.className) {
-                console.log(`message: ClassName:${data.className}, thisClassName:${className}`);
-            }
             if (!data.className || data.className !== className) {
                 return;
             }
             this.setWithTimestamp(...data.arguments, data.timestamp);
         });
-        return instance;
+        return result;
     };
-
-    // copy prototype so intanceof operator still works
-    newConstructor.prototype = original.prototype;
-
-    // return new constructor (will override original)
-    return newConstructor;
+    newConstructorFunction.prototype = constructorFunction.prototype;
+    return newConstructorFunction;
 }
