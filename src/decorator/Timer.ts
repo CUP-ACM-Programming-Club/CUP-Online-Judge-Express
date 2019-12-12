2b597dda294c96bab99d21ba8eb7da13a3c52dee
Ryan Lee <gxlhybh@gmail.com>
2019-12-12 19:44:38 +0800

    [Enhance] Release 3.0.7 Decorator timer opentracing

--- src/decorator/Timer.ts
+++ src/decorator/Timer.ts
@@ -1,10 +1,22 @@
+import http from "http";
+
 export default function Timer(target: any, propertyName: string, propertyDescriptor: PropertyDescriptor) {
     const method = propertyDescriptor.value;
     propertyDescriptor.value = function (...args: any[]) {
+        const httpRequest: any = args[0];
+        let child: any;
+        const useOpenTracing: boolean = httpRequest instanceof http.ClientRequest;
+        if (useOpenTracing) {
+            // @ts-ignore
+            child = httpRequest.tracer.startSpan(`${target.constructor.name}.${propertyName}`, {childOf: httpRequest.parentSpan});
+        }
         const startTimeMill = Date.now();
         const response = method.apply(this, args);
         const endTimeMill = Date.now();
         console.log(`Method: ${propertyName} used ${endTimeMill - startTimeMill}ms.`);
+        if (useOpenTracing) {
+            child.finish(httpRequest);
+        }
         return response;
     }
 }
--- src/middleware/tracer.ts
+++ src/middleware/tracer.ts
@@ -3,11 +3,13 @@ const opentracing = require("@alicloud/opentracing");
 const tracer = new opentracing.Tracer("主链路");
 
 export interface TraceRequest extends Request{
-    parentSpan: any
+    parentSpan: any,
+    tracer: any
 }
 
 export default function (req: TraceRequest, res: Response, next: NextFunction) {
     req.parentSpan = tracer.startSpan("根模块");
+    req.tracer = tracer;
     req.parentSpan.setTag(opentracing.Tags.PEER_HOSTNAME, req.hostname);
     req.parentSpan.setTag(opentracing.Tags.HTTP_METHOD, req.method.toUpperCase());
     req.parentSpan.setTag(opentracing.Tags.HTTP_URL, req.url);
