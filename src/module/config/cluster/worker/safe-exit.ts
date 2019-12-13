a3dd2a9ca526fa2ddb40c10cd2e5b1b4463faa12
Ryan Lee <gxlhybh@gmail.com>
2019-12-13 15:30:48 +0800

    [Fix] Release 3.0.9 Cluster.worker.id is not defined in undefined.

--- src/module/config/cluster/worker/safe-exit.ts
+++ src/module/config/cluster/worker/safe-exit.ts
@@ -2,10 +2,9 @@ const {server} = require("../../../init/http-server");
 const sequelize = require("../../../../orm/instance/sequelize");
 const mysql = require("../../../mysql_query");
 const redis = require("../../../redis");
-const cluster = require("cluster");
 
 module.exports = function () {
-	console.log(`Worker ${cluster.worker.id} close:`);
+	console.log(`Worker PID: ${process.pid} close:`);
 	server.close();
 	console.log("HTTP server is closed");
 	sequelize.close();
@@ -15,7 +14,7 @@ module.exports = function () {
 	redis.quit();
 	console.log("Redis is closed");
 	setTimeout(() => {
-		console.log(`Worker ${cluster.worker.id} execute process.exit(0)`);
+		console.log(`Worker PID: ${process.pid} execute process.exit(0)`);
 		process.exit(0);
 	}, 5000);
 };
