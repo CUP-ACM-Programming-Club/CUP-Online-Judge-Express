9b9dbf09afff8a67367dc8f7a8bcc062e11266a5
Ryan Lee <gxlhybh@gmail.com>
2019-12-13 19:53:03 +0800

    Fix filter

--- src/manager/SolveMap/SolveMapManager.ts
+++ src/manager/SolveMap/SolveMapManager.ts
@@ -76,8 +76,8 @@ export class SolveMapManager {
     @Cacheable(new CachePool(), 1, "day")
     async getSolveMap(problem_id?: number | string) {
         const userList = await this.getUserList();
-        const ACLists = await Promise.all(userList.map((user: any) => this.getUserACListByUserId(user.user_id)));
-        _.filter(ACLists, (e: any) => Array.isArray(e) && e.length > 0);
+        let ACLists: any[] = await Promise.all(userList.map((user: any) => this.getUserACListByUserId(user.user_id)));
+        ACLists = _.filter(ACLists, (e: any) => Array.isArray(e) && e.length > 0);
         const formattedEdges = await Promise.all(ACLists.map(list => this.formatAcceptProblemToEdges(list, problem_id)));
         const edges: any = [];
         formattedEdges.forEach(edgeList => edges.push(...edgeList));
