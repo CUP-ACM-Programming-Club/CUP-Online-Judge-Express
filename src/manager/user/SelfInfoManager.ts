61cf161f0ec1e1ce5e2cc22fc971469012a16637
Ryan Lee <gxlhybh@gmail.com>
2020-01-08 22:43:28 +0800

    [Fix] Release 3.3.6 Contest mode

---
+++ src/manager/user/SelfInfoManager.ts
@@ -0,0 +1,31 @@
+import {Request, Response} from "express";
+import {ErrorHandlerFactory} from "../../decorator/ErrorHandler";
+import {ok} from "../../module/constants/state";
+import _ from "lodash";
+declare global {
+    namespace NodeJS {
+        interface Global {
+            contest_mode: boolean
+        }
+    }
+}
+class SelfInfoManager {
+    @ErrorHandlerFactory(ok.okMaker)
+    getSelfInfo(req: Request) {
+        const sessionCopy = _.cloneDeep(req.session);
+        if (Object.prototype.hasOwnProperty.call(sessionCopy, "captcha")) {
+            delete sessionCopy!.captcha;
+        }
+        return {
+            user_id: req.session!.user_id,
+            nick: req.session!.nick,
+            admin: req.session!.isadmin,
+            avatar: req.session!.avatar,
+            avatarUrl: req.session!.avatarUrl,
+            privilege: sessionCopy,
+            contest_mode: global.contest_mode
+        };
+    }
+}
+
+export default new SelfInfoManager();
