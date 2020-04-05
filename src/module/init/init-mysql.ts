ddb8cbd85482067ca1f989e6c603ab00a6852037
Ryan Lee <gxlhybh@gmail.com>
2020-04-05 22:17:52 +0800

    [Enhance] Release 3.17.1 Add mysql-init script

--- src/module/init/init-mysql.ts
+++ src/module/init/init-mysql.ts
@@ -1,10 +1,11 @@
+import path from "path";
 // @ts-ignore
 const Importer = require("mysql-import");
 export default async function () {
     const init = global.config.init;
     if (!init) {
         const importer = new Importer(global.config.mysql);
-        await importer.import('path/to/dump.sql');
+        await importer.import(`${path.join(process.cwd(), "sql", "structure.sql")}`);
         const files_imported = importer.getImported();
         console.log(`${files_imported.length} SQL file(s) imported.`);
     }
