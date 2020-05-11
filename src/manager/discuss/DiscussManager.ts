c5e25ea5f4ca2de6ddba562ba9ba520e4f9aa23a
Ryan Lee <gxlhybh@gmail.com>
2020-05-11 17:32:05 +0800

    Update discuss API

---
+++ src/manager/discuss/DiscussManager.ts
@@ -0,0 +1,17 @@
+import { Request } from "express";
+const cache_query = require("../../module/mysql_cache");
+class DiscussManager {
+    getArticleInfo () {
+
+    }
+
+    async getUncheckReply () {
+
+    }
+
+    async getUncheckReplyByRequest(req: Request) {
+
+    }
+}
+
+export default new DiscussManager();
