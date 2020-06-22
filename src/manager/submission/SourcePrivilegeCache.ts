bee5c8fc0265e08117198332254a3e341bf561d3
Ryan Lee <gxlhybh@gmail.com>
2020-06-23 01:49:01 +0800

    Prepare ContestAssistant infoHandler privilege

--- src/manager/submission/SourcePrivilegeCache.ts
+++ src/manager/submission/SourcePrivilegeCache.ts
@@ -6,12 +6,12 @@ import CachePool from "../../module/common/CachePool";
 
 class SourcePrivilegeCache {
     @Cacheable(new CachePool(), 10, "second")
-    async checkPrivilege (session: Express.Session, id: number)  {
+    async checkPrivilege (session: Express.Session, solutionId: number)  {
         const basePrivilege = session!.isadmin || session!.source_browser;
         if (basePrivilege) {
             return basePrivilege;
         }
-        const contestId = (await SubmissionManager.getSolutionInfo(id)).contest_id;
+        const contestId = (await SubmissionManager.getSolutionInfo(solutionId)).contest_id;
         if (!contestId) {
             return false;
         }
--- src/module/status/info.js
+++ src/module/status/info.js
@@ -1,3 +1,5 @@
+import SourcePrivilegeCache from "../../manager/submission/SourcePrivilegeCache";
+
 const query = require("../mysql_cache");
 const {error, ok} = require("../constants/state");
 module.exports = async function (req, database, solution_id, own_watch = false) {
@@ -6,7 +8,7 @@ module.exports = async function (req, database, solution_id, own_watch = false) 
 	const solutionPromise = query("select user_id from solution where solution_id = ?", [solution_id]);
 	const [infoRes, solutionRes] = await Promise.all([infoPromise, solutionPromise]);
 	if (solutionRes.length > 0) {
-		if (!req.session.isadmin && (!own_watch || user_id !== solutionRes[0].user_id)) {
+		if (!(req.session.isadmin || await SourcePrivilegeCache.checkPrivilege(req.session, solution_id)) && (!own_watch || user_id !== solutionRes[0].user_id)) {
 			return error.noprivilege;
 		} else {
 			return ok.okMaker({
