881a9ab85a2a8c5257f0c8b37fe8e33a62d028b9
Ryan Lee <gxlhybh@gmail.com>
2020-06-29 18:25:56 +0800

    Modify sequelize addModel

--- src/orm/instance/sequelize.ts
+++ src/orm/instance/sequelize.ts
@@ -1,4 +1,5 @@
 import {Sequelize} from "sequelize-typescript";
+import * as Models from "../ts-model"
 const mysqlConfig = global.config.mysql;
 const sequelize = new Sequelize("jol", mysqlConfig.user, mysqlConfig.password, {
 	host: mysqlConfig.host,
@@ -6,5 +7,5 @@ const sequelize = new Sequelize("jol", mysqlConfig.user, mysqlConfig.password, {
 	timezone: "+08:00"
 });
 sequelize.addModels([process.cwd() + "/dist/orm/ts-model/**/*.ts"])
-
+sequelize.addModels(Object.values(Models));
 export = sequelize;
