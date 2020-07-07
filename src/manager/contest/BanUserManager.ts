8a7d87a567b21db4e9d707be7c1943fc572ca09d
Ryan Lee <gxlhybh@gmail.com>
2020-07-07 15:29:53 +0800

    [Fix] Release 3.20.7 Fix sim check issues.

--- src/manager/contest/BanUserManager.ts
+++ src/manager/contest/BanUserManager.ts
@@ -5,14 +5,16 @@ export class BanUserManager {
 
     private async solutionContainContestId(solutionId: number) {
         const data: any = await SubmissionManager.getSolutionInfo(solutionId);
-        return data && data.length > 0 && data[0].contest_id && !isNaN(parseInt(data[0].contest_id));
+        return data && data.contest_id && !isNaN(parseInt(data.contest_id));
     }
 
     async banUser (solution_pack: any) {
         if (!ConfigManager.isSwitchedOn("ban_contest_cheater", 0)) {
             return;
         }
-        if (parseInt(solution_pack.sim) >= ConfigManager.getConfig("ban_contest_sim_trigger", 100) && solution_pack.state === 4 &&
+        solution_pack.sim = parseInt(solution_pack.sim);
+        const sim = solution_pack.sim;
+        if (sim >= ConfigManager.getConfig("ban_contest_sim_trigger", 100) && solution_pack.state === 4 &&
             (Object.prototype.hasOwnProperty.call(solution_pack, "contest_id") || await this.solutionContainContestId(solution_pack.solution_id))) {
             if (!Object.prototype.hasOwnProperty.call(solution_pack, "contest_id")) {
                 Object.assign(solution_pack, await SubmissionManager.getSolutionInfo(solution_pack.solution_id));
