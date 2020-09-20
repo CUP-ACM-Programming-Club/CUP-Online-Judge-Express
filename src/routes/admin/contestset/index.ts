fd99ad729dc49477239c2f0f53db9c641c95dec8
Ryan Lee <gxlhybh@gmail.com>
2020-09-20 23:00:22 +0800

    [Fix] Release 3.21.2 Add missing parameter.

--- src/routes/admin/contestset/index.ts
+++ src/routes/admin/contestset/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import ContestSetManager from "../../../manager/contestset/ContestSetManager";
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+router.get("/:contestSetId", async (req, res) => {
     res.json(await ContestSetManager.getContestSetForAdministratorByContestSetIdByRequest(req));
 })
 
