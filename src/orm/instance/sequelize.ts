bc2df850637935e6c85e61050afc4c73b21a378e
Ryan Lee <gxlhybh@gmail.com>
2020-12-06 22:44:19 +0800

    [Fix] Release 3.24.3 Add retry option for sequelize

--- src/orm/instance/sequelize.ts
+++ src/orm/instance/sequelize.ts
@@ -5,7 +5,30 @@ const mysqlConfig = global.config.mysql;
 const sequelize = new Sequelize("jol", mysqlConfig.user, mysqlConfig.password, {
 	host: mysqlConfig.host,
 	dialect: "mysql",
-	timezone: "+08:00"
+	timezone: "+08:00",
+	retry  : {
+		match: [
+			/ETIMEDOUT/,
+			/EHOSTUNREACH/,
+			/ECONNRESET/,
+			/ECONNREFUSED/,
+			/ETIMEDOUT/,
+			/ESOCKETTIMEDOUT/,
+			/EHOSTUNREACH/,
+			/EPIPE/,
+			/EAI_AGAIN/,
+			/SequelizeConnectionError/,
+			/SequelizeConnectionRefusedError/,
+			/SequelizeHostNotFoundError/,
+			/SequelizeHostNotReachableError/,
+			/SequelizeInvalidConnectionError/,
+			/SequelizeConnectionTimedOutError/
+		],
+		max  : 5,
+	},
+	dialectOptions: {
+		connectTimeout: 20000, // default is 10s which causes occasional ETIMEDOUT errors (see https://stackoverflow.com/a/52465919/491553)
+	}
 });
 Logger.log(`add sequelize module`);
 sequelize.addModels([process.cwd() + "/dist/orm/ts-model/**/*.ts"])
