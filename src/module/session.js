95a5bb16a8789ab266718906637f08444e408876
Ryan Lee <gxlhybh@gmail.com>
2020-12-25 02:03:16 +0800

    [Release] Release 3.26.3 Experiment add diverse session

--- src/module/session.js
+++ src/module/session.js
@@ -4,16 +4,31 @@ const Random = require("meteor-random");
 const redisClient = require("./redis").default;
 const secretKey = global.config.session_secret || Random.secret(128);
 const oneDay = 86400000;
-const sessionMiddleware = session({
-	store: new RedisStore({ client: redisClient }),
-	saveUninitialized: false,
-	ttl: oneDay * 31,
-	resave: false,
-	secret: secretKey,
-	cookie: Object.assign({
-		maxAge: oneDay * 31
-	}, global.config.cookie)
-});
+const sessionMiddleware = function (req, res, next) {
+	const host = req.get("host");
+	if (typeof host === "string" && host.includes(global.config.cookie.domain || null)) {
+		return session({
+			store: new RedisStore({ client: redisClient }),
+			saveUninitialized: false,
+			ttl: oneDay * 31,
+			resave: false,
+			secret: secretKey,
+			cookie: {
+				maxAge: oneDay * 31
+			}
+		})(req, res, next);
+	}
+	return session({
+		store: new RedisStore({ client: redisClient }),
+		saveUninitialized: false,
+		ttl: oneDay * 31,
+		resave: false,
+		secret: secretKey,
+		cookie: Object.assign({
+			maxAge: oneDay * 31
+		}, global.config.cookie)
+	})(req, res, next);
+};
 
 module.exports.sessionMiddleware = sessionMiddleware;
 module.exports.sessionStore = RedisStore;
