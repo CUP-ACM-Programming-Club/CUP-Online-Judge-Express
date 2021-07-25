74e4c27c4219195cee4d2453ff0bd51f28ade4ab
Ryan Lee <gxlhybh@gmail.com>
2021-07-26 00:07:44 +0800

    fix ts error

--- src/routes/admin/system/log.ts
+++ src/routes/admin/system/log.ts
@@ -3,11 +3,11 @@ import LogManager from "../../../manager/system/LogManager";
 const router = express.Router();
 
 router.get("/out", async (req, res) => {
-    res.json(await LogManager.getStdoutLog(parseInt(req.params.line)))
+    res.json(await LogManager.getStdoutLog(parseInt(req.params!.line! as string)))
 });
 
 router.get("/err", async (req, res) => {
-    res.json(await LogManager.getStderrLog(parseInt(req.params.line)))
+    res.json(await LogManager.getStderrLog(parseInt(req.params!.line! as string)))
 });
 
 export = ["/log", router];
