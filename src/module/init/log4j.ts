37eccf393f00f691cac16a9689963d21b17c298f
Ryan Lee <gxlhybh@gmail.com>
2022-07-16 22:54:42 +0800

    [Release] Release 3.30.5 Fix log4js crash

--- src/module/init/log4j.ts
+++ src/module/init/log4j.ts
@@ -2,7 +2,7 @@ import log4js, {Logger} from "log4js"
 
 log4js.configure({
     appenders: {
-        app: {type: 'dateFile', filename: './log/', pattern: `${global.currentDaemonName || "default"}-yyyy-MM-dd.log`, alwaysIncludePattern: true},
+        app: {type: 'dateFile', filename: './log/log4j-log.log', pattern: `${global.currentDaemonName || "default"}-yyyy-MM-dd.log`, alwaysIncludePattern: true},
         console: {type: "console"}
     },
     categories: {
