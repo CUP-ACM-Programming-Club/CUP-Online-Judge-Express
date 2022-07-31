6c0a9564968d5606f78a941b93c950640bc94dc3
Ryan Lee <gxlhybh@gmail.com>
2022-08-01 00:32:38 +0800

    [Release] Release 3.30.11 Fxi datetime formation

--- src/routes/admin/account/ban_manage.ts
+++ src/routes/admin/account/ban_manage.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const router = express.Router();
 import BanModel from "../../../module/user/ban";
+import dayjs from "dayjs";
 const {error, ok} = require("../../../module/constants/state");
 const {trimProperty} = require("../../../module/util");
 router.get("/", async (req, res) => {
@@ -15,6 +16,7 @@ router.get("/", async (req, res) => {
 router.post("/", async (req, res) => {
 	try {
 		let {user_id, datetime} = trimProperty(req.body);
+		datetime = dayjs(datetime).format("YYYY-MM-DD HH:mm:ss");
 		await BanModel.set(user_id, datetime);
 		res.json(ok.ok);
 	} catch (e) {
